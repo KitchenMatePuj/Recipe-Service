230989b38230354b6e438b6804686d02b6725bfa
MrDesilvestri <124096886+MrDesilvestri@users.noreply.github.com>
2025-05-26 20:43:09 -0500

    Update Application.py

--- src/main/python/Application.py
+++ src/main/python/Application.py
@@ -2,13 +2,12 @@ import json
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware  # <--- Importar CORS
+from fastapi.middleware.cors import CORSMiddleware 
 from src.main.python.config.DatabaseConfig import engine
 from src.main.python.middlewares.log_body import LogRequestBody
 from src.main.python.models import Base
 
 
-# Importar tus routers
 from src.main.python.controller.comment_controller import router as comment_router
 from src.main.python.controller.ingredient_controller import router as ingredient_router
 from src.main.python.controller.category_controller import router as category_router
@@ -16,7 +15,6 @@ from src.main.python.controller.recipe_controller import router as recipe_router
 from src.main.python.controller.recipe_step_controller import router as recipe_step_router
 from src.main.python.utils.responses import UTF8JSONResponse
 
-# Crear tablas
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
@@ -28,18 +26,14 @@ app = FastAPI(
 
 app.add_middleware(LogRequestBody)
 
-# ---------- AÑADIR ESTE BLOQUE PARA HABILITAR CORS ----------
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:4200", "http://localhost:8080"], 
-    # Agrega aquí las URLs que quieras permitir.
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# -------------------------------------------------------------
 
-# Incluir los routers
 app.include_router(comment_router)
 app.include_router(ingredient_router)
 app.include_router(category_router)
